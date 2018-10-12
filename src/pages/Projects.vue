b0d3e17ded35776fd22d026f26f8d3f8f2c3c005
Trevor Orgill <trevororgill@weber.edu>
2018-10-12 12:00:14 -0600

    Initial Skeleton done

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,25 +1,34 @@
 <template>
   <div>
-    <q-tree
-      :nodes="projects"
-      node-key="label"
-      default-expand-all
-    >
-      <div slot="header-generic" slot-scope="prop" class="row items-center">
-        <q-icon :name="prop.node.icon" color="orange" size="28px" class="q-mr-sm" />
-        <div class="text-weight-bold text-primary">{{ prop.node.label }}</div>
-      </div>
-      <div slot="body-generic" slot-scope="prop">
-        <div v-if="prop.node.description">
-          <span class="text-weight-thin">{{ prop.node.description }}</span>
+    <q-card flat>
+      <q-card-title>There are more Projects on Github</q-card-title>
+      <q-card-separator/>
+      <q-tree
+        :nodes="projects"
+        node-key="label"
+        default-expand-all
+      >
+        <div slot="header-generic" slot-scope="prop" class="row items-center">
+          <q-icon :name="prop.node.icon" color="orange" size="28px" class="q-mr-sm" />
+          <div class="text-weight-bold text-primary">{{ prop.node.label }}</div>
         </div>
-        <span v-else class="text-weight-thin">No Description.</span>
-      </div>
-    </q-tree>
+        <div slot="body-generic" slot-scope="prop">
+          <div v-if="prop.node.address">
+            <q-btn flat class="text-weight-thin cursor-pointer" @click="openURL(prop.node.address)">Visit It Here</q-btn>
+          </div>
+          <div v-if="prop.node.description">
+            <span class="text-weight-thin">{{ prop.node.description }}</span>
+          </div>
+          <span v-else class="text-weight-thin">No Description.</span>
+        </div>
+      </q-tree>
+    </q-card>
   </div>
 </template>
 
 <script>
+import { openURL } from 'quasar'
+
 export default {
   name: 'projects',
   data () {
@@ -28,7 +37,7 @@ export default {
         {
           label: 'Work Projects',
           header: 'generic',
-          icon: 'settings',
+          icon: 'work',
           children: [
             {
               label: 'Garrison Transportation',
@@ -41,13 +50,18 @@ export default {
               address: 'https://www.xzam.net',
               description: 'Secure Web Testing Software for Universities',
               body: 'generic'
+            },
+            {
+              label: 'Seed Science',
+              description: 'Science behind different seeds mobile/web app (Not currently published)',
+              body: 'generic'
             }
           ]
         },
         {
           label: 'Personal Projects',
           header: 'generic',
-          icon: '',
+          icon: 'person',
           children: [
             {
               label: 'Discord Bot (Raffle Bot)',
@@ -55,17 +69,23 @@ export default {
               body: 'generic'
             },
             {
-              label: 'Good table presentation',
+              label: 'Raffle',
+              address: 'https://flinoge.github.io/Raffle/',
+              description: 'A Website Originally Used as the Raffle-Bot (Key no longer valid)',
               body: 'generic'
             },
             {
-              label: 'Pleasing decor',
+              label: 'Misc Games on Unreal Engine',
+              description: 'Done tutorials and experimented with my own games on unreal engine.',
               body: 'generic'
             }
           ]
         }
       ]
     }
+  },
+  methods: {
+    openURL
   }
 }
 </script>
